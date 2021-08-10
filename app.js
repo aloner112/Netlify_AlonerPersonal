22e0b2a0ca2459e14d1c847efaa0b22e2e307ac6
Aloner <aloner112@gmail.com>
2021-08-10 15:45:34 +0800

    feat: try load db

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-db.collection('PlayerDatas').get().then( (snapshot) => {
-    console.log(snapshot.docs);
+const playerDataList = document.querySelector('#playerDataList');
+
+function fillPlayersData(doc){
+    let playerName = doc.data().PlayerName;
+    let guildName = doc.data().GuildName;
+    let teams = doc.data().Teams;
+    let comment = doc.data().Comment;
+
+    let div = document.createElement('div');
+    let title = document.createElement('h3');
+    title.textContent = playerName;
+    let pGuild = document.createElement('p');
+    pGuild.textContent = '公會名：' + guildName;
+    let pComment = document.createElement('p');
+    pComment.textContent = '備註：' + comment;
+
+    div.appendChild(title);
+    div.appendChild(pGuild);
+    div.appendChild(pComment);
+    playerDataList.appendChild(div);
+}
+
+db.collection('PlayerDatas').get().then( snapshot => {
+    snapshot.docs.forEach(doc => {
+        fillPlayersData(doc);
+    });
 });
+/*
+var gotPlayerData = async () => {
+    let snapshot = await db.collection('PlayerDatas').get();
+    snapshot.docs.forEach(doc => {
+        fillPlayersData(doc);
+    });
+}
+
+
