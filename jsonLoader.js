9527968eef76812abe7f561e7f6d0a917dac8450
Aloner <52912342+aloner112@users.noreply.github.com>
2023-05-31 03:06:18 +0800

    access database

--- jsonLoader.js
+++ jsonLoader.js
@@ -1,14 +1,74 @@
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
+import {getDatabase, ref, set, get, child, onValue, update, remove, push
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-database.js";
 import { app, auth } from "/auth.js";
 
 auth.onAuthStateChanged(()=>{
     if(auth.currentUser){
-        displayData();
+        GetData();
     }
     else $('#data').empty();
 });
 
-var database = getDatabase();
+var db = getDatabase();
+var refDramas = "projects/testProject01/dramas";
+var dramas;
+
+var emptyDrama = {
+    type: "drama",
+    dramaName: "New Drama",
+    dramaOrder: "00",
+    dateInStory: "date in story",
+    dialogues:{},
+}
+
+var testJson2 = {
+    projects:{
+        projectKey1:{
+            type: "project",
+            projectName: "projectA",
+            dramas:{
+                dramaKey01:{
+                    type: "drama",
+                    dramaOrder: "01",
+                    dramaName: "dramaNameA",
+                    dateInStory: "2020-01-02",
+                    dialogues:{
+                        dialogueKey01:{
+                            dialogueOrder: "01", 
+                            type: "label",
+                            labelName: "Label01"
+                        },
+                        dialogueKey02:{
+                            dialogueOrder: "02", 
+                            type: "talk",
+                            speaker: "Aloner",
+                            emotion: "Angry",
+                            speech: "Hello, is this Shishiro?"
+                        },
+                        dialogueKey03:{
+                            dialogueOrder: "03", 
+                            type: "talk",
+                            speaker: "Hsuante",
+                            emotion: "Smile",
+                            speech: "No, this is Mr. H."
+                        },
+                        dialogueKey04:{
+                            dialogueOrder: "04", 
+                            type: "endDrama",
+                        }
+                    }
+                }
+            }
+
+        },
+        projectKey2:{
+            
+        },
+        projectKey3:{
+            
+        }
+    }
+}
 
 var testJson = {
     page1:{
@@ -36,41 +96,33 @@ var testJson = {
 
 var dramaKeys = [];
 
-$(document).ready(function () {
-    // var dataDiv = $('#data')
-    // var sidebarDiv = $('<div>').addClass('sidebar');
-    // var dataContentDiv = $('<div>').addClass('dataContent');
-    // dramaKeys = getTypeKeysInJson(testJson, 'drama');
-    // let firstKey = 0;
-    // dramaKeys.forEach(dramaKey => {
-    //     if(firstKey == 0) firstKey = dramaKey;
-    //     let currentItem = testJson[dramaKey];
-    //     let pagingDiv = $('<div>').addClass('paging');
-    //     pagingDiv.html(currentItem.name + '<br>' + currentItem.time);
-    //     pagingDiv.attr('data-key', dramaKey);
-    //     pagingDivAddEvent(pagingDiv);
-    //     $(pagingDiv).appendTo(sidebarDiv);
-    // });
-    // $(sidebarDiv).append(addNewDramaPagingButton());
-    // $(dataDiv).append(sidebarDiv);
-    // $(dataDiv).append(dataContentDiv);
-    // let firstPagingDiv = $('.paging[data-key="' + firstKey +'"]');
-    // pagingClickHandler(firstPagingDiv);
-});
+// $(document).ready(function () {});
 
-function displayData(){
+function GetData(){
+    get(ref(db, refDramas)).then((snapshot) =>{
+        if(snapshot.exists()){
+            dramas = snapshot.val();
+            DisplayData();
+        }else{
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    })    
+}
+
+function DisplayData(){
     var dataDiv = $('#data')
     var sidebarDiv = $('<div>').addClass('sidebar');
     var dataContentDiv = $('<div>').addClass('dataContent');
-    // dataDiv.text(Object.keys(testJson).length.toString());
-    // dataDiv.text(testJson.page1.name);
-    dramaKeys = getTypeKeysInJson(testJson, 'drama');
+    dataContentDiv.attr('id', 'dataContent');
+    dramaKeys = getTypeKeysInJson(dramas, 'drama');
     let firstKey = 0;
     dramaKeys.forEach(dramaKey => {
         if(firstKey == 0) firstKey = dramaKey;
-        let currentItem = testJson[dramaKey];
+        let currentItem = dramas[dramaKey];
         let pagingDiv = $('<div>').addClass('paging');
-        pagingDiv.html(currentItem.name + '<br>' + currentItem.time);
+        pagingDiv.html(currentItem.dramaName + '<br>' + currentItem.dateInStory);
         pagingDiv.attr('data-key', dramaKey);
         pagingDivAddEvent(pagingDiv);
         $(pagingDiv).appendTo(sidebarDiv);
@@ -92,29 +144,29 @@ function pagingClickHandler(pagingDiv){
     $('.paging').removeClass('selected');
     $(pagingDiv).addClass('selected');
     let key = $(pagingDiv).attr('data-key');
-    let data = testJson[key];
-    let dataContent = data.content.replace(/\n/g, '<br>');
-    $('.dataContent').html(dataContent);
+    let data = dramas[key];
+    let dramaOrder = "<div>Drama Order: " + data.dramaOrder + "</div>";
+    let dramaName = "<div>Drama Name: " + data.dramaName + "</div>";
+    let dateInStory = "<div>Date in story: " + data.dateInStory + "</div>";
+    // let dialogues = "<div>" + data.dialogues.replace(/\n/g, '<br>') + "</div>";
+    let content = dramaOrder + dramaName + dateInStory;
+    $('#dataContent').html(content);
 }
 
 function addNewDramaPagingButton(){
     let pagingDiv = $('<div>').addClass('paging');
     pagingDiv.html('Add new drama');
+    pagingDiv.click(()=> addNewDrama() );
     return pagingDiv;
 }
 
 function addNewDrama(){
-
+    let pushRef = push(ref(db, refDramas));
+    set(pushRef, emptyDrama);
 }
 
-function getTypeKeysInJson(jsonObj, targetType){
-    let tmpDramaKeys = [];
-    // 遍歷 JSON 物件的每個屬性
-    for (let key in jsonObj) {
-        if(jsonObj.hasOwnProperty(key) == false) continue;
-        let item = jsonObj[key];
-        if (item.type !== targetType) continue;
-        tmpDramaKeys.push(key);
-    }
+function getTypeKeysInJson(dataObj, targetType){
+    let tmpDramaKeys = Object.keys(dataObj).filter(
+        key => dataObj[key].type == targetType);
     return tmpDramaKeys;
 }
